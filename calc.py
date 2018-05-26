c19d02d15a4198c615f57a0f9b086282638005f2
Wilson Carpenter <wilsonkcarpenter@gmail.com>
2018-05-26 14:05:49 -0700

    Updated calc to add useless functions.

--- calc.py
+++ calc.py
@@ -3,4 +3,19 @@
 print "This is a test program."
 print "123."
 
+def add(x,y):
+	pass
+
+def subtrac(x,y):
+	pass
+
+def multiply(x, y):
+	pass
+
+def divide(x,y):
+	pass
+
+def square(x):
+	pass
+
 
